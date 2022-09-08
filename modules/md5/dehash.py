985aa558ec02f331a0ed7abeecf03e4694046ac0
pigeon <96277381+p-on@users.noreply.github.com>
2022-09-08 21:31:21 +0100

    Update dehash.py

--- modules/md5/dehash.py
+++ modules/md5/dehash.py
@@ -1,14 +1,10 @@
 from main import *
 
-result = None
-
 try:
     hashfile = open(__settings__["hash_files"] + margs[1][:2] + "/" + margs[1][2:4] + ".txt", encoding="latin-1")
     hashes = hashfile.read().splitlines()
     for hash in hashes:
         if hash.__contains__(margs[1] + ":"): 
-            result = hash.split(":")[1]
+            results.append(hash.split(":")[1])
 except Exception as e:
-    result = f"\033[31m{e}"
-
-log("*", f"\033[93m{result}")
+    results.append(f"\033[31m{e}")
