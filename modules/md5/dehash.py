0b9986658a54c8f04f13b084a5f3e24f8e117c52
pigeon <96277381+p-on@users.noreply.github.com>
2022-09-08 16:58:23 +0100

    Create dehash.py

---
+++ modules/md5/dehash.py
@@ -0,0 +1,15 @@
+from main import *
+import base64
+
+result = None
+
+try:
+    hashfile = open(__settings__["hash_files"] + margs[1][:2] + "/" + margs[1][2:4] + ".txt", encoding="latin-1")
+    hashes = hashfile.read().splitlines()
+    for hashx in hashes:
+        if hashx.__contains__(margs[1] + ":"): 
+            result = hashx.split(":")[1]
+except Exception as e:
+    result = f"\033[31m{e}"
+
+log("*", f"\033[93m{result}")
