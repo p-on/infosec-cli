b2edb4d5951fbf6c0839276467b1449c413496bb
pigeon <96277381+p-on@users.noreply.github.com>
2022-09-08 21:30:19 +0100

    Update decode.py

--- modules/base64/decode.py
+++ modules/base64/decode.py
@@ -1,12 +1,8 @@
 from main import *
 import base64
 
-result = None
-
 try:
     del margs[0]
-    result = base64.b64decode(" ".join(margs)).decode('utf-8', 'ignore').replace('`', ' ')
+    results.append(base64.b64decode(" ".join(margs)).decode('utf-8', 'ignore').replace('`', ' '))
 except Exception as e:
-    result = f"\033[31m{e}"
-
-log("*", f"\033[93m{result}")
+    results.append(f"\033[31m{e}")
