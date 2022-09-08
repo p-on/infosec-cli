d06b96d619b75409aabdda002459c8cecaf2f465
pigeon <96277381+p-on@users.noreply.github.com>
2022-09-08 16:20:52 +0100

    Update encode.py

--- modules/base64/encode.py
+++ modules/base64/encode.py
@@ -4,7 +4,8 @@ import base64
 result = None
 
 try:
-    result = base64.b64encode(margs[1].encode()).decode('utf-8', 'ignore')
+    del margs[0]
+    result = base64.b64encode(" ".join(margs).encode()).decode('utf-8', 'ignore')
 except Exception as e:
     result = f"\033[31m{e}"
 
