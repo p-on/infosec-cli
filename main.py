ccf8590ff8e4adf75c872987de5b6a7d979a5a0b
pigeon <96277381+p-on@users.noreply.github.com>
2022-09-08 20:45:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import argparse, os, json, re
+import argparse, os, json, re, threading, pkgutil, importlib, sys
 
 __filepath__ = os.path.dirname(__file__)
 __dirtree__ = os.listdir(__filepath__ + "/modules")
@@ -20,6 +20,35 @@ def __validate_guess__(email_domain: str, domain: str) -> bool:
             return False
 
     return True
+
+def import_submodules(package, recursive=True):
+    if isinstance(package, str):
+        package = importlib.import_module(package)
+    resultsx = {}
+    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
+        full_name = package.__name__ + '.' + name
+        resultsx[full_name] = importlib.import_module(full_name)
+        if recursive and is_pkg:
+            resultsx.update(import_submodules(full_name))
+    return resultsx
+
+def get_functions(modules, args=None):
+    websites = []
+
+    for module in modules:
+        if len(module.split(".")) > 3 :
+            modu = modules[module]
+            site = module.split(".")[-1]
+            if args !=None and args.nopasswordrecovery==True:
+                if  "adobe" not in str(modu.__dict__[site]) and "mail_ru" not in str(modu.__dict__[site]) and "odnoklassniki" not in str(modu.__dict__[site]):
+                    websites.append(modu.__dict__[site])
+            else:
+                websites.append(modu.__dict__[site])
+    return websites
+
+async def launch_module(module, email, client, out):
+    try: await module(email, client, out)
+    except: pass
 
 def log(to: str, what: str):
     if to == "*": to = "\033[93m*"
@@ -48,6 +77,10 @@ def main():
         help = "modules for email int"
     ) # email command, no more to say
 
+    parser.add_argument("-f", "--fails", action="store_true",
+        help = "show fails if any occur in given command"
+    )
+
     args = parser.parse_args()
     sargs = vars(args)
 
@@ -55,7 +88,7 @@ def main():
         margs = sargs[sarg]
         __all__ = [name for name in globals()]
 
-        if margs != None:
+        if type(margs) == list and margs != None:
             print(f"\033[0m[\033[32m+\033[0m] started command \033[93m{sarg}\033[0m")
             try:
                 exec(open(__filepath__ + "/modules/" + sarg + "/" + margs[0] + ".py").read())
