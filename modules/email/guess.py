164eae9978bbc6bf63806e1f8b0e381a21646ab2
pigeon <96277381+p-on@users.noreply.github.com>
2022-09-08 20:46:36 +0100

    Update guess.py

--- modules/email/guess.py
+++ modules/email/guess.py
@@ -1,5 +1,5 @@
 from main import *
-import requests
+import httpx
 
 # credit to IRIS-Team
 
@@ -10,11 +10,11 @@ try:
         margs[1] = margs[1].split("@")[1]
 
     if bool(re.fullmatch(DOMAIN_FORMAT, margs[1])):
-        with requests.get("https://raw.githubusercontent.com/IRIS-Team/IRIS/main/data/domains.txt") as request:
-            email_domains = [x.strip() for x in request.text.splitlines() if len(x.strip()) > 0]
-            for domain in email_domains:
-                if __validate_guess__(str(margs[1]), domain) is True:
-                    results.append(str(domain))
+        request = httpx.get("https://raw.githubusercontent.com/IRIS-Team/IRIS/main/data/domains.txt")
+        email_domains = [x.strip() for x in request.text.splitlines() if len(x.strip()) > 0]
+        for domain in email_domains:
+            if __validate_guess__(str(margs[1]), domain) is True:
+                results.append(str(domain))
     else:
         results = [f"\033[31mdomain not formatted properly"]
 except Exception as e:
