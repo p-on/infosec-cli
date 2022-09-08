03301ad0afd0ebe8af66a19aa7bd3ae6a421a5ea
pigeon <96277381+p-on@users.noreply.github.com>
2022-09-08 15:58:46 +0100

    Update decode.py

--- modules/base64/decode.py
+++ modules/base64/decode.py
@@ -1,1 +1,11 @@
+from main import *
+import base64
 
+result = None
+
+try:
+    result = base64.b64decode(margs[1]).decode('utf-8', 'ignore').replace('`', ' ')
+except Exception as e:
+    result = f"\033[31m{e}"
+
+log("*", f"\033[93m{result}")
