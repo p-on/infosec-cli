bfdf8fd322b39d1a22f444260e32b68390806a08
pigeon <96277381+p-on@users.noreply.github.com>
2022-09-08 21:31:35 +0100

    Update hash.py

--- modules/md5/hash.py
+++ modules/md5/hash.py
@@ -1,11 +1,7 @@
 from main import *
 import hashlib
 
-result = None
-
 try:
-    result = hashlib.md5(margs[1].encode()).hexdigest()
+    results.append(hashlib.md5(margs[1].encode()).hexdigest())
 except Exception as e:
-    result = f"\033[31m{e}"
-
-log("*", f"\033[93m{result}")
+    results.append(f"\033[31m{e}")
