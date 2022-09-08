90dea8e170e2c581bf1705803bc098b83123db5b
pigeon <96277381+p-on@users.noreply.github.com>
2022-09-08 15:03:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-# import ...
+import argparse, os
 
-def main() -> None:
-    pass
+parser = argparse.ArgumentParser(description="A command line interface for infosec.")
+args = parser.parse_args()
 
-if __name__ == "__main__": main() # written with <3 by pigeon & 7ap
+__all__ = [name for name in globals()]
+__filepath__ = os.path.dirname(__file__)
+__dirtree__ = os.listdir(__filepath__ + "/modules")
+
+def main():
+    if args[0] != None:
+        exec(open(__filepath__ + "/modules/" + args[0] + ".py").read())
+        print(f"\033[0m    Loaded command module: \033[93m{args[0]}\033[0m")
+
+if __name__ == "__main__": 
+    main()
