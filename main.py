90f6580fcfc855543962748e239b262f832bae6c
pigeon <96277381+p-on@users.noreply.github.com>
2022-09-08 15:43:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,19 @@ parser.add_argument("-b64", "--base64", type = str, nargs = 2,
 )
 
 args = parser.parse_args()
+sargs = vars(args)
 
-__all__ = [name for name in globals()]
 __filepath__ = os.path.dirname(__file__)
 __dirtree__ = os.listdir(__filepath__ + "/modules")
 
 def main():
-    sargs = vars(args)
-
     for sarg in sargs:
-        if len(sargs[sarg]) > 1:
+        margs = sargs[sarg]
+        __all__ = [name for name in globals()]
+        
+        if len(margs) > 1:
             print(f"\033[0m[\033[32m+\033[0m] started command \033[93m{sarg}\033[0m")
-            exec(open(__filepath__ + "/modules/" + sarg + "/" + sargs[sarg][0] + ".py").read())
+            exec(open(__filepath__ + "/modules/" + sarg + "/" + margs[0] + ".py").read())
 
 if __name__ == "__main__": 
     main()
