4d00005d231456e9ea77fa599cef91ed633232f9
pigeon <96277381+p-on@users.noreply.github.com>
2022-09-08 16:10:21 +0100

    Update encode.py

--- modules/base64/encode.py
+++ modules/base64/encode.py
@@ -1,1 +1,11 @@
+from main import *
+import base64
 
+result = None
+
+try:
+    result = base64.b64encode(margs[1].encode()).decode('utf-8', 'ignore')
+except Exception as e:
+    result = f"\033[31m{e}"
+
+log("*", f"\033[93m{result}")
