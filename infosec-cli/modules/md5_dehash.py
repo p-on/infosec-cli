f841355744515327303d9c6519c7a9cf783fafd7
pigeon <96277381+p-on@users.noreply.github.com>
2022-09-09 09:53:03 +0100

    fixed up md5

--- infosec-cli/modules/md5_dehash.py
+++ infosec-cli/modules/md5_dehash.py
@@ -1,10 +1,10 @@
 from __main__ import *
 
 try:
-    hashfile = open(__settings__["hash_files"] + margs[1][:2] + "/" + margs[1][2:4] + ".txt", encoding="latin-1")
+    hashfile = open(__settings__["hash_files"] + margs[0][:2] + "/" + margs[0][2:4] + ".txt", encoding="latin-1")
     hashes = hashfile.read().splitlines()
     for hash in hashes:
-        if hash.__contains__(margs[1] + ":"): 
+        if hash.__contains__(margs[0] + ":"): 
             results.append(hash.split(":")[1])
 except Exception as e:
     results.append(f"\033[31m{e}")
