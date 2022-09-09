ed1a02795d5cafba978767fb1754a642ba63c801
pigeon <96277381+p-on@users.noreply.github.com>
2022-09-09 09:26:26 +0100

    Delete modules directory

--- modules/base64/encode.py
+++
@@ -1,8 +0,0 @@
-from main import *
-import base64
-
-try:
-    del margs[0]
-    results.append(base64.b64encode(" ".join(margs).encode()).decode('utf-8', 'ignore'))
-except Exception as e:
-    results.append(f"\033[31m{e}")
