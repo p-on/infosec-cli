c0a37b1c8a6a06ce8533687f58c71d6039a472d7
7ap <109433659+7ap@users.noreply.github.com>
2022-09-08 21:57:51 -0500

    feat: stub out the argparse stuff a bit more

--- osint.py/__main__.py
+++ osint.py/__main__.py
@@ -2,8 +2,13 @@ import argparse
 
 def main() -> None:
     parser = argparse.ArgumentParser()
+    parser.prog = "osint.py"
+    parser.description = "A suite of tools condensed into a Python script to facilitate the collection and analysis of data gathered from open sources to produce actionable intelligence."
+    parser.epilog = "pigeon & 7ap were here!"
 
-    ...
+    # parser.add_argument( ... )
+    # parser.add_argument( ... )
+    # parser.add_argument( ... )
 
     args = parser.parse_args()
 
