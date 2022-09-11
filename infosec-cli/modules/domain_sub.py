3c8e3c70ec3666cabe92b861d03b576a8e2041e3
pigeon <96277381+p-on@users.noreply.github.com>
2022-09-11 22:24:27 +0100

    feat: domain force format

--- infosec-cli/modules/domain_sub.py
+++ infosec-cli/modules/domain_sub.py
@@ -2,37 +2,34 @@ from __main__ import *
 import httpx
 
 try:
-    if bool(re.fullmatch(DOMAIN_FORMAT, margs[0])):
-        try:
-            response = httpx.get(f"https://crt.sh/?q={margs[0]}").content
-            links = re.compile("<TD>(.*?)</TD>").findall(str(response))
-            for item in links:
-                subdomains = []
-                item = item.strip()
-                if "<BR>" in item: 
-                    subdomains = item.split("<BR>")
-                else:
-                    subdomains.append(item)
-                for subdomain in subdomains:
-                    subdomain = subdomain.strip()
-                    if not subdomain.endswith("." + margs[0]):
-                        continue
-                    if not subdomain in results:
-                        results.append(subdomain)
-        except: pass
-        try:
-            response = httpx.get(f"https://api.sublist3r.com/search.php?domain={margs[0]}").json()
-            for item in response:
-                if not item in results:
-                    results.append(item)
-        except: pass
-        try:
-            response = httpx.get(f"https://www.threatcrowd.org/searchApi/v2/domain/report/?domain={margs[0]}").json()["subdomains"]
-            for item in response:
-                if not item in results:
-                    results.append(item)
-        except: pass
-    else:
-        results.append(f"\033[31mdomain not formatted properly")
+    try:
+        response = httpx.get(f"https://crt.sh/?q={margs[0]}").content
+        links = re.compile("<TD>(.*?)</TD>").findall(str(response))
+        for item in links:
+            subdomains = []
+            item = item.strip()
+            if "<BR>" in item: 
+                subdomains = item.split("<BR>")
+            else:
+                subdomains.append(item)
+            for subdomain in subdomains:
+                subdomain = subdomain.strip()
+                if not subdomain.endswith("." + margs[0]):
+                    continue
+                if not subdomain in results:
+                    results.append(subdomain)
+    except: pass
+    try:
+        response = httpx.get(f"https://api.sublist3r.com/search.php?domain={margs[0]}").json()
+        for item in response:
+            if not item in results:
+                results.append(item)
+    except: pass
+    try:
+        response = httpx.get(f"https://www.threatcrowd.org/searchApi/v2/domain/report/?domain={margs[0]}").json()["subdomains"]
+        for item in response:
+            if not item in results:
+                results.append(item)
+    except: pass
 except Exception as e:
     results.append(f"\033[31m{e}")
